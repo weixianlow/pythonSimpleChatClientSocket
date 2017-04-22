18ea920263725b1fb650fb7fd954ee2c5c0d801d
Wei Xian Low <weix.low@gmail.com>
2017-04-21 22:49:20 -0500

    Added comment and header

--- src/client.py
+++ src/client.py
@@ -1,5 +1,14 @@
+#Name: Wei Xian Low
+#CMP_SC4850
+#title: Lab3
+#Description: This is the client side of the chat application which allows the user to connect to the server and receive messages and responses from the server. This application will forward all user's command and message to the user.
+
+
+
+#nesscary library
 import socket, select, string, sys
- 
+
+#function for prompt to user on when to type
 def prompt() :
     sys.stdout.write('<You> ')
     sys.stdout.flush()
@@ -7,13 +16,12 @@ def prompt() :
 #main function
 if __name__ == "__main__":
      
-    if(len(sys.argv) < 3) :
-        print 'Usage : python telnet.py hostname port'
-        sys.exit()
-     
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-     
+    
+    #client connection configuration 
+    host = 'localhost'
+    port = 15100
+    
+    #enable socket object and timeout 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
      
@@ -21,10 +29,13 @@ if __name__ == "__main__":
     try :
         s.connect((host, port))
     except :
+        #if fail
         print 'Unable to connect'
         sys.exit()
-     
-    print 'Connected to remote host.'
+
+    #startup message
+    print 'My Chat Room Client V2\n' 
+    print 'Connected to server at '+ host + ', port number: '+ str(port) +'.'
     prompt()
      
     while 1:
@@ -33,20 +44,23 @@ if __name__ == "__main__":
         # Get the list sockets which are readable
         read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
          
+        #to handle incomming message
         for sock in read_sockets:
             #incoming message from remote server
             if sock == s:
                 data = sock.recv(4096)
                 if not data :
+                    #if data didn't contain anything, and received no response, assume server has been disconnected and proceed to exit
                     print '\nDisconnected from chat server'
                     sys.exit()
                 else :
-                    #print data
+                    #print data from server
                     sys.stdout.write(data)
                     prompt()
              
             #user entered a message
             else :
+                #send the message to the server for processing
                 msg = sys.stdin.readline()
                 s.send(msg)
                 prompt()
